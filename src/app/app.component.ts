4dfd83da3a5fe6352073ebd52303d1d1e948486b
Vladimir Drayling <draylegend@gmail.com>
2023-11-28 00:30:50 +0100

    feat(myApp): display angular version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myApp';
+  version = packageJson.dependencies['@angular/core'];
 }
